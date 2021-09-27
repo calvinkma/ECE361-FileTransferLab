3b6b129c731b6eb1cd66c31fac005c741b9e0134
Calvin Ma <calvinkma@outlook.com>
2021-09-27 14:35:13 -0400

    Add round_trip_time_test to measure round trip time.

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC=gcc
-all: server deliver server_test
+all: server deliver server_test round_trip_time_test
 server: server.o
 deliver: deliver.o
 clean:
-	rm -f *.o server deliver server_test server-*
+	rm -f *.o server deliver server_test server-* round_trip_time_test
--- round_trip_time_test.c
+++ round_trip_time_test.c
@@ -6,12 +6,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
 
 
 #define MAXLINE 1024 // The maximum number of bytes that can be received from one UDP packet
 
 /*
-    This file tests the functionality of server.c by sending three packets from one file.
+    This file tests the round trip time by ending one packet to server, and waiting for an `ACK` reply.
     Run the test by `./server_test <server_ip> <server_port_number>`, e.g. `./server_test 128.100.13.158 3000`.
     On the server side, a file `foobar.txt` should be created.
 */
@@ -23,6 +24,7 @@ int main(int argc, char *argv[]) {
     char message[10];
     uint32_t server_addr_binary;
     short success;
+    struct timeval tval_before, tval_after, tval_result;
 
      // Check input, must be supplied exactly 2 arguments
     if (argc != 3) {
@@ -45,44 +47,37 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // Prepare metadata for sending
     server_port = (unsigned short)(atoi(argv[2]));
-
-    // printf("Server address binary: %lu\n", server_addr_binary);
-    // printf("Server port: %u\n", server_port);
-
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(server_port);
     server_addr.sin_addr.s_addr = server_addr_binary;
+    char frag0[23] = "1:1:6:foobar.txt:H Worl";
 
-    // Create test messages
-    char frag0[23] = "3:1:6:foobar.txt:H Worl";
-    char frag1[23] = "3:2:6:foobar.txt:d\0from";
-    char frag2[21] = "3:3:6:foobar.txt:Test";
+    // Prepare metadata for receiving
+    int n_bytes_received;
+    char buffer[MAXLINE];
+    int server_addr_size;
 
-    // printf("socket_fd: %d\n", socket_fd);
-    // printf("message: %s\n", message);
-    // printf("strlen(message): %d\n", strlen(message));
+    // Start timer
+    gettimeofday(&tval_before, NULL);
 
     // Send a message
     int n_bytes_sent = sendto(socket_fd, (char *)frag0, 23, 0, (struct sockaddr*)(&server_addr), sizeof(server_addr));
     printf("%d bytes sent succesfully.\n", n_bytes_sent);
 
-    n_bytes_sent = sendto(socket_fd, (char *)frag1, 23, 0, (struct sockaddr*)(&server_addr), sizeof(server_addr));
-    printf("%d bytes sent succesfully.\n", n_bytes_sent);
 
-    n_bytes_sent = sendto(socket_fd, (char *)frag2, 21, 0, (struct sockaddr*)(&server_addr), sizeof(server_addr));
-    printf("%d bytes sent succesfully.\n", n_bytes_sent);
+    // Receive a message
+    n_bytes_received = recvfrom(socket_fd, buffer, MAXLINE, 0, (struct sockaddr *)(&server_addr), &server_addr_size);
 
-    // Receive 3 messages
-    for (int i = 0; i < 3; i++) {
-        int n_bytes_received;
-        char buffer[MAXLINE];
-        int server_addr_size;
-        n_bytes_received = recvfrom(socket_fd, buffer, MAXLINE, 0, (struct sockaddr *)(&server_addr), &server_addr_size);
-        buffer[n_bytes_received] = '\0';
-
-        printf("Server : %s\n", buffer);
-    }
+    // End timer
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+    printf("Time elapsed: %ld.%06ld seconds.\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+    
+    // Process received message
+    buffer[n_bytes_received] = '\0';
+    printf("Server : %s\n", buffer);
 
     return 0;
 }
